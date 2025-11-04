525e888615c1523a0b89ced418b4c633a5f9e406
manu <manu@gmail.com>
2025-11-04 22:49:07 +0530

    initial commit

---
+++ deliveryboy/src/main/java/com/delivery/deliveryboy/config/AppConstant.java
@@ -0,0 +1,5 @@
+package com.delivery.deliveryboy.config;
+
+public class AppConstant {
+    public static final String LOCATION_TPOIC_NAME="location-update-topic";
+}
---
+++ enduser/src/main/java/com/enduser/enduser/AppConstant.java
@@ -0,0 +1,8 @@
+package com.enduser.enduser;
+
+public class AppConstant {
+
+    public static final String LOCATION_TPOIC_NAME="location-update-topic";
+
+    public static final String GROUP_ID="group-1";
+}
